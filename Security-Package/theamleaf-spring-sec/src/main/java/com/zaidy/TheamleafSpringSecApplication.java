dac3ee31030df77b03aa054b2dcd9b96baefeeaf
Seydi Abubakre <aboubakreseydi@gmail.com>
2022-10-06 21:45:25 +0000

    ok

---
+++ Security-Package/theamleaf-spring-sec/src/main/java/com/zaidy/TheamleafSpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.zaidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheamleafSpringSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheamleafSpringSecApplication.class, args);
+    }
+
+}
---
+++ Security-Package/theamleaf-spring-sec/src/test/java/com/zaidy/TheamleafSpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheamleafSpringSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
